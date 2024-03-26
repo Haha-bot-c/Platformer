469c32f3c73479b9d6c63def76d57fe22664f546
Haha-bot-c <94055850+Haha-bot-c@users.noreply.github.com>
2024-03-26 19:13:20 +0700

    TriggerAttak.cs

--- Assets/Skrupt/TriggerAttak.cs
+++ Assets/Skrupt/TriggerAttak.cs
@@ -1,36 +1,52 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(Health))]
 public abstract class TriggerAttak : MonoBehaviour
 {
-    protected const float NullIndex = 0f;
-
-    [SerializeField] protected float _attackRange = 10f;
     [SerializeField] protected float _attackDamage = 10f;
     [SerializeField] protected float _attackCooldown = 1f;
 
-    protected List<Collider2D> _charactersInRange = new();
+    protected Collider2D _nearestCharacterInRange = new();
     protected Coroutine _attack = null;
 
     protected virtual void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<Health>())
+        if (other.TryGetComponent(out Health healthComponent))
         {
-            _charactersInRange.Add(other); 
+            UpdateNearestCharacterInRange(other);
         }
     }
-
+    
     protected virtual void OnTriggerExit2D(Collider2D other)
-    {
-        if (_charactersInRange.Contains(other))
+    { 
+        if (other == _nearestCharacterInRange)
         {
-            _charactersInRange.Remove(other);
+            _nearestCharacterInRange = null;
         }
 
-        if (_charactersInRange.Count == NullIndex && _attack != null)
+        if (_attack != null)
         {
             StopCoroutine(_attack);
             _attack = null;
+        }
+    }
+
+    private void UpdateNearestCharacterInRange(Collider2D other)
+    {
+        if (_nearestCharacterInRange == null)
+        {
+            _nearestCharacterInRange = other;
+        }
+        else
+        {
+            float currentDistance = Vector2.Distance(transform.position, _nearestCharacterInRange.transform.position);
+            float newDistance = Vector2.Distance(transform.position, other.transform.position);
+
+            if (newDistance < currentDistance)
+            {
+                _nearestCharacterInRange = other;
+            }
         }
     }
 }
