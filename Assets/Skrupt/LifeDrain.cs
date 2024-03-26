8e7d8d9db23c1eff6aef670952b88f3d35b40f77
Haha-bot-c <94055850+Haha-bot-c@users.noreply.github.com>
2024-03-26 19:14:06 +0700

    LifeDrain.cs

--- Assets/Skrupt/LifeDrain.cs
+++ Assets/Skrupt/LifeDrain.cs
@@ -1,9 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-
-[RequireComponent(typeof(Health))]
-
 public class LifeDrain : TriggerAttak
 {
     [SerializeField] private float _duration = 6f;
@@ -12,29 +9,29 @@ public class LifeDrain : TriggerAttak
     {
         if (_attack == null && Input.GetKeyDown(KeyCode.E))
         {
-            _attack = StartCoroutine(AttackCoroutine());
+            _attack = StartCoroutine(Attack());
         }
     }
 
-    private IEnumerator AttackCoroutine()
+    private IEnumerator Attack()
     {
         float elapsedTime = 0f;
-        
+
         WaitForSeconds wait = new WaitForSeconds(_attackCooldown);
 
-        while (_charactersInRange.Count > NullIndex && elapsedTime < _duration)
+        Health thisHealth = GetComponent<Health>();
+        Health character = _nearestCharacterInRange.GetComponent<Health>();
+
+        while (_nearestCharacterInRange != null && elapsedTime < _duration)
         {
-            foreach (Collider2D character in _charactersInRange)
-            {
-                character.GetComponent<Health>().TakeDamage(_attackDamage);
-                GetComponent<Health>().Heal(_attackDamage);
-            }
+            character.GetComponent<Health>().TakeDamage(_attackDamage);
+            thisHealth.Heal(_attackDamage);
             
             yield return wait;
 
-            elapsedTime += _attackCooldown; 
-            
-            if(elapsedTime >= _duration)
+            elapsedTime += _attackCooldown;
+
+            if (elapsedTime >= _duration)
                 _attack = null;
         }
     }
